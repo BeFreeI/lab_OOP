85b018ef494f36dcabcbc201b79162b1e82d9e88
BeFreeI <nepogodin_p@mail.ru>
2020-09-24 17:36:03 +0300

    some things from lab8

---
+++ lab8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab8
+{
+    class EightNum
+    {
+        private string num;
+        public EightNum()
+        {
+            num = "0";
+        }
+        public EightNum(string number)
+        {
+            foreach(char el in number)
+            {
+                if (Convert.ToInt32(el) >= 7)
+                {
+                    throw new Exception("Unexpected character");
+                }
+            }
+            num = number;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert.ToInt32("4") == 4);
+        }
+    }
+}
