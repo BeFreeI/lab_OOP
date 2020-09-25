893d47897aec78bc152190285af33dc2e10caae3
BeFreeI <nepogodin_p@mail.ru>
2020-09-25 16:57:47 +0300

    lab5 completed

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -1,37 +1,54 @@
 ﻿using System;
 
-namespace lab6
-{
-    class Program
-    {
-        static public int StrangeTask1(int[,] arr)
-        {
+namespace lab6 {
+    class Program {
+        static public int StrangeTask1(int[,] arr) {
             int rez = 0,
                 sum = 0;
             bool flag = false;
-            for(int j = 0; j < arr.GetLength(1); j++)
-            {
+            for (int j = 0; j < arr.GetLength(1); j++) {
                 sum = 0;
                 flag = false;
-                for (int i = 0; i < arr.GetLength(0); i++)
-                {
+                for (int i = 0; i < arr.GetLength(0); i++) {
                     sum += arr[i, j];
-                    if(arr[i, j] < 0)
-                    {
+                    if (arr[i, j] < 0) {
                         flag = true;
                     }
                 }
-                if(!flag)
-                {
+                if (!flag) {
                     rez += sum;
                 }
             }
             return rez;
         }
-        static void Main(string[] args)
-        {
-            int[,] arr = { { 1, -2, 3 }, { 4, 5, 6 }, { 7, 8, -9 }, { 10, 11, -12 } };
-            Console.WriteLine(StrangeTask1(arr));
+
+        static public int StrangeTask2(int[,] arr) {
+            int sum = int.MaxValue,
+                helpSum = 0,
+                size = arr.GetLength(0);
+            for (int val = 0; val < size; val++) {
+                helpSum = 0;
+                for(int i = val, j = 0; i >= 0; i--, j++) {
+                    helpSum += Math.Abs(arr[i, j]);
+                }
+                sum = Math.Min(helpSum, sum);
+            }
+            for (int val = 1; val < size; val++) {
+                helpSum = 0;
+                for(int i = size - 1, j = val; j < size; j++, i--) {
+                    helpSum += Math.Abs(arr[i, j]);
+                }
+                sum = Math.Min(helpSum, sum);
+            }
+            return sum;
+        }
+
+        static void Main(string[] args) {
+            int[,] arr = { { 11, 12, 13, 14 }, 
+                           { 21, 22, 23, 24 }, 
+                           { 31, 32, 33, 34 }, 
+                           { 41, 42, 43, 44 } };
+            StrangeTask2(arr);
         }
     }
 }
---
+++ lab9/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace lab9 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }                                      
+    }                                          
+}
